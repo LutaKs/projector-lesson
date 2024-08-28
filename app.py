48a92184e5b7239a4c1b32b4106dd6b9f7b4d209
LutaKs <lutaya.kseniya90@gmail.com>
2024-08-28 23:09:28 +0300

    add div

--- app.py
+++ app.py
@@ -2,14 +2,15 @@ print('enter first number:')
 s = input()
 print('enter second number:')
 s2 = input()
-print('choose operation: +/-')
+print('choose operation: + - /')
 op = input()
 match op:
     case "+":
-        print(int(s)+ int(s2))
+        print(int(s) + int(s2))
 
     case "-":
         print(int(s) - int(s2))
-
+    case "/":
+        print(int(s) / int(s2))
     case _:
         print("unknown operation")
