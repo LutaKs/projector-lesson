d64feb9b9822b14ac58e6a5ccaec15d04ef1cbb9
LutaKs <lutaya.kseniya90@gmail.com>
2024-08-28 23:04:19 +0300

    add mult

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ print('enter first number:')
 s = input()
 print('enter second number:')
 s2 = input()
-print('choose operation: +/-')
+print('choose operation: + - *')
 op = input()
 match op:
     case "+":
@@ -10,6 +10,7 @@ match op:
 
     case "-":
         print(int(s) - int(s2))
-
+    case "*":
+        print(int(s) * int(s2))
     case _:
         print("unknown operation")
