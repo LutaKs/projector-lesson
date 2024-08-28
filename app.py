86bb10e65519c426fdcdc5cac075bf0ae6b54eaa
LutaKs <lutaya.kseniya90@gmail.com>
2024-08-28 22:58:41 +0300

    improve cli

--- app.py
+++ app.py
@@ -1,2 +1,15 @@
+print('enter first number:')
 s = input()
-print(eval(s))
+print('enter second number:')
+s2 = input()
+print('choose operation: +/-')
+op = input()
+match op:
+    case "+":
+        print(int(s)+ int(s2))
+
+    case "-":
+        print(int(s) - int(s2))
+
+    case _:
+        print("unknown operation")
